d24ddc3754cae020a9d4d48b71321e603ac73ec6
omarpecos <omarpecos@gmail.com>
2021-05-11 19:48:46 +0200

    Added env

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,21 @@
 <template>
-  <a-scene>
-    <a-entity
-      geometry="primitive:box;"
-      material="shader:gif;src:url(https://media2.giphy.com/media/l0MYrDjuf8DN9lcWY/giphy.gif?cid=790b7611cdf820a1931f7e9dcee325ddde4784f0ea8b78de&rid=giphy.gif&ct=g);color:green;opacity:.8"
-    ></a-entity>
-  </a-scene>
+  <div>
+    <h1>{{ config.apiUrl }}</h1>
+  </div>
 </template>
 
 <script>
+import { environment } from "./util.js";
+import { ref } from "vue";
+
 export default {
   name: "App",
+  setup() {
+    const config = ref(environment);
+
+    return { config };
+  },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/util.js
@@ -0,0 +1,6 @@
+// get config and export it
+
+export const environment = {
+  myAccountPass: process.env.VUE_APP_ACCOUNT_PASS,
+  apiUrl: process.env.VUE_APP_API_URL,
+};
